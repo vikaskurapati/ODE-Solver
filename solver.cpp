732bb0ca0682696cac7fa509660e717036e974fb
Nikhita K Venkat <nikhita.venkat@tum.de>
2021-11-16 18:58:02 +0100

    Modified function to catch errors

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <math.h>
 #include "solver.h"
 
 std::vector<double> solver(const std::function<double(double, double)>& f, double int_value, double t, double dt)
@@ -11,10 +12,13 @@ std::vector<double> solver(const std::function<double(double, double)>& f, doubl
     double current_t = 0;
     while(current_t < t)
     {
+        if (isnan(gradient) || isinf(gradient)||isnan(solution.back())||isnan(solution.back()))
+        {
+            throw(std::invalid_argument("Gradient or Solution not a number, please check your function analytically"));
+        }
         gradient = f(solution.back(),current_t);
         solution.push_back(solution.back() + dt*gradient);
         current_t += dt;
     }
     return solution;
 }
-
