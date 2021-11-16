b1f68f3b7dfffc35e581a70bca5f0674dd3991ee
Nikhita K Venkat <nikhita.venkat@tum.de>
2021-11-16 18:57:38 +0100

    Changed the main.cpp file to incorporate a lower dt

--- main.cpp
+++ main.cpp
@@ -9,10 +9,12 @@ const double pi = 3.1415;
 
 int main()
 {
-    auto expo = [](double x, double t){ return exp(t);};
+    // the gradient is to be input as a lamba function as a function of x, t. x is the variable being solved for and t is the independent variable
+    // eg. dx/dt = e^t is show here. dx/dt = e^x would return exp(x)
+    auto expo = [](double x, double t){ return 1;};
     std::vector<double> solution;
     // solver is defined as solver(function, intial_value, final_time, time_step)
-    solution = solver(expo, 1.0, 2.0, 0.0002);
+    solution = solver(expo, 1.0, 2.0, 0.002);
     std::cout << solution.back() << std::endl;
     return 0;
 }
