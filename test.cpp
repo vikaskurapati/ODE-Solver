78d0063c0385f2c2e354f4ac282e193794ee927a
Vikas Kurapati <vikas.kurapati@tum.de>
2021-11-18 21:24:47 +0100

    Changed the test framework using Catch2

--- test.cpp
+++ test.cpp
@@ -1,3 +1,5 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include <iostream>
 #include <functional>
 #include <cmath>
@@ -6,33 +8,15 @@
 #include<fstream>
 #include <cassert>
 
-void test_solver()
-{
+
+TEST_CASE( "Solver is tested", "[solver]" ) {
     auto test1 = [](double x, double t){ return 1;};
     auto test2 = [](double x, double t){ return exp(t);};
         
     auto solution1 = solver(test1, 0.0, 2.0, 0.002);
     auto solution2 = solver(test2, 0.0, 2.0, 0.002);
 
-    bool error = false;
+    REQUIRE( solution1.back()-2.0 < 0.001 );
+    REQUIRE( solution2.back()-(exp(2)-1) < 0.001);
 
-    if (solution1.back()-2.0 > 0.001)
-    {
-        std::cout << " Error in Test Case1" << std::endl;
-        error = true;
-    }
-    if (solution2.back()-(exp(2)-1) > 0.001)
-    {
-        std::cout << "Error in Test Case2" << std::endl;
-        error = true;
-    }
-    if (!error)
-    {
-        std::cout << "No Errors, Test Cases ran succesfully, Hurraaahhhhh!!!" << std::endl;
-    }
-}
-
-int main(void)
-{
-    test_solver();
 }
