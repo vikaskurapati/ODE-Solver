7a54b37eb5804bdbfa55447cd42e9bd823599c39
Nikhita K Venkat <nikhita.venkat@tum.de>
2021-11-20 16:13:54 +0100

    Added variables for better user readability and printed the solution

--- main.cpp
+++ main.cpp
@@ -12,9 +12,12 @@ int main()
     // the gradient is to be input as a lamba function as a function of x, t. x is the variable being solved for and t is the independent variable
     // eg. dx/dt = e^t is show here. dx/dt = e^x would return exp(x)
     auto expo = [](double x, double t){ return 1;};
+    double intial_value = 1.0;
+    double final_time = 2.0;
+    double time_step = 0.1;
     std::vector<double> solution;
     // solver is defined as solver(function, intial_value, final_time, time_step)
-    solution = solver(expo, 1.0, 2.0, 0.002);
-    std::cout << solution.back() << std::endl;
+    solution = solver(expo, intial_value, final_time, time_step);
+    print_solution(solution);
     return 0;
 }
