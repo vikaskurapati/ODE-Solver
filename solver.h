135b2f53fb997e3fef4fac5baee692420962870f
Vikas Kurapati <vikas.kurapati@tum.de>
2021-11-22 14:28:22 +0100

    Removed the include iostream and added JAVADOC format documentation on the header. Need to check this again

--- solver.h
+++ solver.h
@@ -1,4 +1,11 @@
-#include <iostream>
+/**
+* Explicit Euler scheme to solve an ODE
+*
+* Compute the solution of an ODE on the basis of the given gradient function using
+* Explicit Euler Scheme
+*
+* @param[in] f the function to use, int_value the initial value of the function, t the time until which the solver needs to solve, dt the timestep of the function
+*/
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 
