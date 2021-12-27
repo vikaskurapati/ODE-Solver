f32ed3c977f4fb11ab841fd4d318abd812bdcdbb
Vikas Kurapati <vikas.kurapati@tum.de>
2021-12-27 22:25:58 +0100

    Replace expo by gradient_function variable

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     // the gradient is to be input as a lamba function which is a function of x, t. x is the variable being solved for and t is the independent variable
     // eg. dx/dt = e^t is show here. dx/dt = e^x would return exp(x)
-    auto expo = [](double x, double t){ return 1;};
+    auto gradient_function = [](double x, double t){ return 1;};
     double initial_value = 1.0;
     double final_time = 2.0;
     double time_step = 0.1;
@@ -19,7 +19,7 @@ int main()
     std::cout << "Provide the timestep to be used" << std::endl;
     std::cin >> time_step;
     // solver is defined as solver(function, initial_value, final_time, time_step)
-    solution = solver(expo, initial_value, final_time, time_step);
+    solution = solver(gradient_function, initial_value, final_time, time_step);
     print_solution(solution);
     return 0;
 }
